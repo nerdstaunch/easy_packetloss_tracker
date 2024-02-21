c0d90b0b3e4aabf5b1aa1366c450db4b02a50532
Nerdstaunch <flamingmagma@gmail.com>
2024-02-22 09:51:37 +1100

    Allows logging to output file

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Run with .\script.ps1 -target "google.com"
 
 -target defaults to google.com
+
+Log to a file with:
+.\script.ps1 > output.txt
--- script.ps1
+++ script.ps1
@@ -10,6 +10,7 @@ $processed = 0
 $allTimeouts = @() 
 $start = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+Write-Output "$timestamp - Pinging $target ..."
 Write-Host "$timestamp - Pinging $target ..."
 
 try {
@@ -21,15 +22,19 @@ try {
             $successCount++
             $pingReply = Test-Connection -ComputerName $target -Count 1
             if ($pingReply -ne $null) {
+                Write-Output "[$($processed+1)/$count] $timestamp - Reply from $($pingReply.Address): bytes=$($pingReply.BufferSize) time=$($pingReply.ResponseTime)ms TTL=$($pingReply.TimeToLive)"
                 Write-Host "[$($processed+1)/$count] $timestamp - Reply from $($pingReply.Address): bytes=$($pingReply.BufferSize) time=$($pingReply.ResponseTime)ms TTL=$($pingReply.TimeToLive)"
             } else {
                 $timeoutCount++
+                Write-Output "[$($processed+1)/$count] $timestamp - Request timed out"
                 Write-Host "[$($processed+1)/$count] $timestamp - Request timed out"
                 $allTimeouts += "[$($processed+1)/$count] $timestamp"  # Add timestamp to array
             }
         } else {
             $timeoutCount++
+            Write-Output "[$($processed+1)/$count] $timestamp - Request timed out."
             Write-Host "[$($processed+1)/$count] $timestamp - Request timed out."
+
             $allTimeouts += "[$($processed+1)/$count] $timestamp"  # Add timestamp to array
         }
 
@@ -38,6 +43,16 @@ try {
 } finally {
     $packetLoss = [Math]::Round(($timeoutCount / $processed) * 100, 2)
     $totalTimeouts = $timeoutCount
+    Write-Output " "
+    Write-Output "===== Summary ====="
+    Write-Output "From: $start || To: $timestamp"
+    Write-Output "Total Packets sent: $processed"
+    Write-Output "Packet loss: $packetLoss%"
+    Write-Output "Total timeouts: $totalTimeouts"
+    Write-Output " "
+    Write-Output "== All Timeouts: =="
+
+
     Write-Host " "
     Write-Host "===== Summary ====="
     Write-Host "From: $start || To: $timestamp"
@@ -46,11 +61,15 @@ try {
     Write-Host "Total timeouts: $totalTimeouts"
     Write-Host " "
     Write-Host "== All Timeouts: =="
+
     # Print array of timestamps for timeouts
     foreach ($timeout in $allTimeouts) {
+        Write-Output $timeout
         Write-Host $timeout
     }
+    Write-Output "==================="
     Write-Host "==================="
+
 
 
     # Wait for key press to prevent premature exit
